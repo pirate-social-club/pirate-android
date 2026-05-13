194b14de2dba904c0323f9ea752e2743a96a2c30
technohippi3 <technohippi3@users.noreply.github.com>
2026-05-13 09:57:34 +0400

    Bump Android Play release version to 9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,8 +60,8 @@ android {
     applicationId = "sc.pirate.app"
     minSdk = 28
     targetSdk = 36
-    versionCode = 8
-    versionName = "0.1.0-alpha.6-play-20260513"
+    versionCode = 9
+    versionName = "0.1.0-alpha.7-play-20260513"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
